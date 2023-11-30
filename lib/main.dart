502ed395b96a39e0428549165f89bcc9c44f30bd
GABRIEL WINICIUS COSTA CORREA <correa.6980@aluno.pr.senac.br>
2023-11-30 15:19:31 -0300

    Implementação do botão implementar task e ocultar lista de tarefas (AnimatedOpacity)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+   MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
 
   @override
   Widget build(BuildContext context) {
@@ -16,21 +23,33 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
           appBar: AppBar(
-            title: const Text('Tarefas'),
+            title: Text('Tarefas'),
+            leading: Icon(Icons.add_task),
           ),
-          body: ListView(
-            children: [
-              Task('Aprender Flutter no intervalo',
-                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',1),
-              Task('Andar De Bike',
-                  'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',2),
-              Task('Ler',
-                  'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',3),
-              Task('Meditar',
-                  'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',4),
-            ],
+          body: AnimatedOpacity(
+            opacity: opacidade? 1.0 : 0.0,
+            duration: Duration(seconds: 1),
+            child: ListView(
+              children: [
+                Task('Aprender Flutter no intervalo',
+                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',1),
+                Task('Andar De Bike',
+                    'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',2),
+                Task('Ler',
+                    'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',3),
+                Task('Meditar',
+                    'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',4),
+              ],
+            ),
           ),
-          floatingActionButton: FloatingActionButton(onPressed: () {})),
+          floatingActionButton: FloatingActionButton(onPressed: () {
+            setState(() {
+              opacidade = !opacidade;
+            });
+          },
+            child: Icon(Icons.remove_red_eye),
+          )
+      ),
     );
   }
 }
@@ -57,24 +76,37 @@ class _TaskState extends State<Task> {
         child: Stack(
           children: [
             Container(
-              color: Colors.lightBlueAccent,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: Colors.lightBlueAccent
+              ),
               height: 140,
             ),
             Column(
               children: [
                 Container(
-                  color: Colors.white,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    color: Colors.white,
+                  ),
+
                   height: 100,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Container(
-                        color: Colors.black26,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          color: Colors.black26,
+                        ),
                         width: 80,
                         height: 100,
-                        child: Image.network(
-                          widget.foto,
-                          fit: BoxFit.cover,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: Image.network(
+                            widget.foto,
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
                       Column(
